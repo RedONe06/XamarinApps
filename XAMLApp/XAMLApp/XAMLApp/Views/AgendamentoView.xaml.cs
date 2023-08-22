e73d68e42df5bbb412522837c22cd8713e4c8784
Andriele Joras dos Santos <andrielejorasdossantos@gmail.com>
2023-08-22 09:08:08 -0300

    :rocket: Final class

--- XAMLApp/XAMLApp/XAMLApp/Views/AgendamentoView.xaml.cs
+++ XAMLApp/XAMLApp/XAMLApp/Views/AgendamentoView.xaml.cs
@@ -1,23 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace XAMLApp.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AgendamentoView : ContentPage
-	{
-		public Veiculo Veiculo { get; set; }
-		public AgendamentoView (Veiculo veiculo)
-		{
-			InitializeComponent ();
-			this.Veiculo = veiculo;
-			this.BindingContext = this;
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AgendamentoView : ContentPage
+    {
+        public Veiculo Veiculo { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+
+        DateTime dataAgendamento = DateTime.Today;
+        public DateTime DataAgendamento
+        {
+            get
+            {
+                return dataAgendamento;
+            }
+            set
+            {
+                dataAgendamento = value;
+            }
+        }
+
+        public TimeSpan HoraAgendamento { get; set; }
+
+
+        public AgendamentoView(Veiculo veiculo)
+        {
+            InitializeComponent();
+            this.Veiculo = veiculo;
+            this.BindingContext = this;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Agendamento",
+                string.Format("Nome: {0}\nFone: {1}\nE-mail: {2}\nData Agendamento: {3}\nHora Agendamento: {4}", Nome, Telefone, Email, DataAgendamento.ToString("dd/MM/yyyy"), HoraAgendamento), "Ok");
+        }
+    }
 }
--- XAMLApp/XAMLApp/XAMLApp/Views/DetalhesView.xaml.cs
+++ XAMLApp/XAMLApp/XAMLApp/Views/DetalhesView.xaml.cs
@@ -1,29 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace XAMLApp.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class DetalhesView : ContentPage
-	{
-		public Veiculo Veiculo { get; set; }
-		public DetalhesView (Veiculo veiculo)
-		{
-			InitializeComponent ();
-			this.Veiculo = veiculo;
-			this.BindingContext = this;
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalhesView : ContentPage
+    {
+        private const int FREIO_ABS = 800;
+        private const int AR_CONDICIONADO = 1000;
+        private const int MP3_PLAYER = 500;
+
+        public Veiculo Veiculo { get; set; }
+        public string TextoFreioABS
+        {
+            get
+            {
+                return string.Format("Freio ABS - R$ {0}", FREIO_ABS);
+            }
+        }
+        private bool temFreioABS;
+        public bool TemFreioABS
+        {
+            get { return temFreioABS; }
+            set
+            {
+                temFreioABS = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar Condicionado - R$ {0}", AR_CONDICIONADO);
+            }
+        }
+
+        private bool temArCondicionado;
+        public bool TemArCondicionado
+        {
+            get { return temArCondicionado; }
+            set
+            {
+                temArCondicionado = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        public string TextoMP3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player - R$ {0}", MP3_PLAYER);
+            }
+        }
+        private bool temMP3Player;
+        public bool TemMP3Player
+        {
+            get { return temMP3Player; }
+            set
+            {
+                temMP3Player = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public string ValorTotal
+        {
+            get
+            {
+                return string.Format("Valor total: R$ {0}", Veiculo.Preco + (TemFreioABS ? FREIO_ABS : 0) + (TemArCondicionado ? AR_CONDICIONADO : 0) + (TemMP3Player ? MP3_PLAYER : 0));
+            }
+        }
+
+        public DetalhesView(Veiculo veiculo)
+        {
+            InitializeComponent();
+            this.Veiculo = veiculo;
+            this.BindingContext = this;
+        }
 
         private void buttonProximo_Clicked(object sender, EventArgs e)
         {
-			
-			Navigation.PushAsync(new XAMLApp.Views.AgendamentoView(this.Veiculo));
+
+            Navigation.PushAsync(new XAMLApp.Views.AgendamentoView(this.Veiculo));
         }
     }
 }
